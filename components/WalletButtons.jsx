f0fe19aedb52944d817943b7bb9a59c0df471704
Bernard Onuh <onuhbernard4@gmail.com>
2023-06-15 02:12:31 +0100

    wallet

--- components/WalletButtons.jsx
+++ components/WalletButtons.jsx
@@ -1,40 +1,20 @@
+import React from "react";
 import {
-    useWallet,
-    WalletReadyState,
-    Wallet,
-    isRedirectable,
-  } from "@aptos-labs/wallet-adapter-react";
-  
-  const WalletButtons = () => {
-    const { wallets } = useWallet();
-  
-    return (
-      <>
-        {wallets.map((wallet) => {
-          return walletView(wallet);
-        })}
-      </>
-    );
-  };
-  
+  useWallet,
+  WalletReadyState,
+  isRedirectable,
+} from "@aptos-labs/wallet-adapter-react";
+
+const WalletButtons = () => {
+  const { wallets, connect } = useWallet();
+
   const walletView = (wallet) => {
-    const { connect } = useWallet();
     const isWalletReady =
       wallet.readyState === WalletReadyState.Installed ||
       wallet.readyState === WalletReadyState.Loadable;
     const mobileSupport = wallet.deeplinkProvider;
-    /**
-     * If we are on a mobile browser, adapter checks whether a wallet has a `deeplinkProvider` property
-     * a. If it does, on connect it should redirect the user to the app by using the wallet's deeplink url
-     * b. If it does not, up to the dapp to choose on the UI, but can simply disable the button
-     * c. If we are already in a in-app browser, we dont want to redirect anywhere, so connect should work as expected in the mobile app.
-     *
-     * !isWalletReady - ignore installed/sdk wallets that dont rely on window injection
-     * isRedirectable() - are we on mobile AND not in an in-app browser
-     * mobileSupport - does wallet have deeplinkProvider property? i.e does it support a mobile app
-     */
+
     if (!isWalletReady && isRedirectable()) {
-      // wallet has mobile app
       if (mobileSupport) {
         return (
           <button
@@ -47,7 +27,6 @@ import {
           </button>
         );
       }
-      // wallet does not have mobile app
       return (
         <button
           className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mr-4 opacity-50 cursor-not-allowed`}
@@ -58,7 +37,6 @@ import {
         </button>
       );
     } else {
-      // we are on desktop view
       return (
         <button
           className={`bg-blue-500  text-white font-bold py-2 px-4 rounded mr-4 ${
@@ -73,4 +51,8 @@ import {
       );
     }
   };
-  export default WalletButtons;
+
+  return <>{wallets.map((wallet) => walletView(wallet))}</>;
+};
+
+export default WalletButtons;
